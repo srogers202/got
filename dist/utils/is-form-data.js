b8361a2467347ca2764d3981bdcceda9b22d0e44
Seth Rogers <seth.rogers@infusionsoft.com>
2019-02-28 17:11:54 -0700

    remove dist

--- dist/utils/is-form-data.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var is_1 = require("@sindresorhus/is");
-exports.default = (function (body) { return is_1.default.nodeStream(body) && is_1.default.function_(body.getBoundary); });
--- dist/utils/supports-brotli.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var zlib_1 = require("zlib");
-exports.default = typeof zlib_1.default.createBrotliDecompress === 'function';
